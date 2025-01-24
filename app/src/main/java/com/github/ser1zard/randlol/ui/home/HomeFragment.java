1ceba6b48362b4d70aa8795862026114db180be4
Ser1zard <stefano.cozzolino74@gmail.com>
2025-01-24 13:26:36 +0100

    Implemented GENERATE button inside the Home Fragment.

--- app/src/main/java/com/github/ser1zard/randlol/ui/home/HomeFragment.java
+++ app/src/main/java/com/github/ser1zard/randlol/ui/home/HomeFragment.java
@@ -4,16 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.github.ser1zard.randlol.R;
 import com.github.ser1zard.randlol.databinding.FragmentHomeBinding;
+
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    private final HomeViewModel homeViewModel = new HomeViewModel();
     private FragmentHomeBinding binding;
+    private List<String> randomPick;
+    private Button random;
+    private ImageView championRandom, laneRandom;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -22,6 +31,20 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        championRandom = root.findViewById(R.id.champion_random);
+        laneRandom = root.findViewById(R.id.lane_random);
+
+        random = root.findViewById(R.id.random);
+        random.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                randomPick = homeViewModel.getRandom(getContext());
+
+                championRandom.setImageResource(getResources().getIdentifier(randomPick.get(1), "drawable", getContext().getPackageName()));
+                laneRandom.setImageResource(getResources().getIdentifier(randomPick.get(0), "drawable", getContext().getPackageName()));
+            }
+        });
 
         return root;
     }
--- app/src/main/java/com/github/ser1zard/randlol/ui/home/HomeViewModel.java
+++ app/src/main/java/com/github/ser1zard/randlol/ui/home/HomeViewModel.java
@@ -1,19 +1,83 @@
 package com.github.ser1zard.randlol.ui.home;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
 import androidx.lifecycle.ViewModel;
+
+import com.github.ser1zard.randlol.dao.DatabaseHelper;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 public class HomeViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private DatabaseHelper dbHelper;
+    private SQLiteDatabase db;
+    private Cursor cursor;
 
-    public HomeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
-    }
+    public List<String> getRandom(Context context) {
+        LinkedHashMap<String, String> lanes = new LinkedHashMap<>();
 
-    public LiveData<String> getText() {
-        return mText;
+        dbHelper = new DatabaseHelper(context);
+        db = dbHelper.getReadableDatabase();
+
+        try {
+            cursor = db.rawQuery("SELECT * FROM lanes", null);
+
+            if (cursor.moveToFirst()) {
+                do {
+                    lanes.put(cursor.getString(0), cursor.getString(1));
+                } while (cursor.moveToNext());
+            }
+        } finally {
+            if (cursor != null && !cursor.isClosed()) {
+                cursor.close();
+            }
+            dbHelper.close();
+        }
+
+        LinkedHashMap<String, String> champions = new LinkedHashMap<>();
+
+        dbHelper = new DatabaseHelper(context);
+        db = dbHelper.getReadableDatabase();
+
+        try {
+            cursor = db.rawQuery("SELECT * FROM champions", null);
+
+            if (cursor.moveToFirst()) {
+                do {
+                    champions.put(cursor.getString(0), cursor.getString(1));
+                } while (cursor.moveToNext());
+            }
+        } finally {
+            if (cursor != null && !cursor.isClosed()) {
+                cursor.close();
+            }
+            dbHelper.close();
+        }
+
+        List<String> randomPick = new ArrayList<>();
+        Random random = new Random();
+
+        List<Map.Entry<String, String>> lanesList = new ArrayList<>(lanes.entrySet());
+        List<Map.Entry<String, String>> championsList = new ArrayList<>(champions.entrySet());
+
+        Map.Entry<String, String> lane = lanesList.get(random.nextInt(lanesList.size()));
+        Map.Entry<String, String> champion = championsList.get(random.nextInt(championsList.size()));
+
+        Log.d("DEBUG", lane + " " + champion);
+
+        randomPick.add(lane.getValue());
+        randomPick.add(champion.getValue());
+
+        Log.d("DEBUG", randomPick.toString());
+
+        return randomPick;
     }
 }
